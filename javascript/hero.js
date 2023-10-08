53f3b389d48561da9e92114c6fbabf3cbe10766f
Gopi8939 <gopiaswin2002@gmail.com>
2023-10-08 17:55:58 +0530

    new code

---
+++ javascript/hero.js
@@ -0,0 +1,24 @@
+$("#myCarousel .carousel-item").each(function () {
+  var minPerSlide = 3;
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+    next.children(":first-child").clone().appendTo($(this));
+  }
+});
+$(document).ready(function () {
+  $("#myCarousel").on("slid.bs.carousel", function () {
+    var activeItem = $(this).find(".carousel-item.active");
+    // activeItem.addClass('active-carousel-item');
+    var tetContent = activeItem.find(".tet").html();
+    $("#madhu").html(tetContent);
+  });
+});
